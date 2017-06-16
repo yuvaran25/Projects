22cff3dc4accadb014e4e5bd2a1ec908569a62bc
sivasankarreddyy <sankar.siva486@gmail.com>
2017-06-16 15:59:12 -0300

    Update DataExchange.java

--- Lejos/src/org/sankar/DataExchange.java
+++ Lejos/src/org/sankar/DataExchange.java
@@ -3,6 +3,7 @@ package org.sankar;
 import java.io.DataInputStream;
 import java.io.IOException;
 
+//This class is used to store the information of distance measured by the ultrasonic sensor and the input received from the controller
 public class DataExchange {
 	
 	public static int transmitReceived;
